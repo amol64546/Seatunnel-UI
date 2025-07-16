1466e45a70af384ddc0bb98161b4e4f1bf44dcdc
Amol N <amol64546@gmail.com>
2025-07-16 18:12:49 +0530

    refactor

--- src/components/EnvConfigPanel.jsx
+++ src/components/EnvConfigPanel.jsx
@@ -6,7 +6,7 @@ export const defaultEnvConfig = {
   "job.retry.times": 3,
   "job.retry.interval.seconds": 3,
   "checkpoint.interval": 30000,
-  "checkpoint.timeout": 30000,  
+  "checkpoint.timeout": 300000,  
   "read_limit.rows_per_second": 400,
   "read_limit.bytes_per_second": 7000000
 };
